438efebfba5625853b055938f3bf299639c9780d
Lazar Zivadinovic <lzivadinovic@itekako.com>
2020-12-09 20:41:55 +0100

    First day

---
+++ 01_12/second_part.py
@@ -0,0 +1,7 @@
+import numpy as np
+from itertools import combinations
+data = np.loadtxt('input1')
+for i in combinations(data,3):
+    if sum(i) == 2020:
+        print(i[0]*i[1]*i[2])
+        break
