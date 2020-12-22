d12661de416c1add363492cc0d85f2269403ff4a
Lazar Zivadinovic <lzivadinovic@itekako.com>
2020-12-22 13:01:37 +0100

    Day 4: Update first part short solution

--- 04_12/first_part_short.py
+++ 04_12/first_part_short.py
@@ -3,7 +3,6 @@ import re
 # just parse data nicely
 data = open('input4', 'r').read().split('\n\n')
 req_f = set(['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'])
-#r = map(lambda line: 1 if req_f.issubset(set(re.split(':| ',line.replace('\n', ' '))[::2])), data)
-data = [ 1 for line in data if req_f.issubset(set(re.split(':| ',line.replace('\n', ' '))[::2]))]
+valid = sum([req_f.issubset(set(re.split(':| ',line.replace('\n', ' '))[::2])) for line in data ]) 
 
-print(len(data))
+print(valid)
